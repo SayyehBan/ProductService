a4845ab13200de01e3c4ca6092bcddeef3d5eba1
SayyehBan <sdvp1991david@gmail.com>
2024-03-28 16:29:43 +0330

    Add project files.

---
+++ ProductService/Model/Services/ICategoryService.cs
@@ -0,0 +1,49 @@
+﻿using ProductService.Infrastructure.Contexts;
+using ProductService.Model.Entities;
+
+namespace ProductService.Model.Services;
+
+public interface ICategoryService
+{
+    List<CategoryDto> GetCategories();
+    void AddNewCatrgory(CategoryDto category);
+}
+public class RCategoryService : ICategoryService
+{
+    private readonly ProductDatabaseContext context;
+
+    public RCategoryService(ProductDatabaseContext context)
+    {
+        this.context = context;
+    }
+
+    public void AddNewCatrgory(CategoryDto category)
+    {
+        Category newCategory = new Category
+        {
+            Description = category.Description,
+            Name = category.Name,
+        };
+        context.Categories.Add(newCategory);
+        context.SaveChanges();
+    }
+
+    public List<CategoryDto> GetCategories()
+    {
+        var data = context.Categories
+           .OrderBy(p => p.Name)
+           .Select(p => new CategoryDto
+           {
+               Description = p.Description,
+               Name = p.Name,
+               Id = p.Id
+           }).ToList();
+        return data;
+    }
+}
+public class CategoryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ ProductService/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Infrastructure.Contexts;
+using SayyehBanTools.ConnectionDB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ProductDatabaseContext>(p=>p.UseSqlServer(SqlServerConnection.ConnectionString("4Mgp5catJqMnBNvqAqdJ2w==", "eWyP6NKkWfiTzk6B1pz8gw==", "m6UQxl628s/a1Hx1CxA2LQ==", "xbfQyKCUrBvw5zxn8sMOfg==", "257ld6s4dsc16e2j", "69q18j991xl48u6u")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
